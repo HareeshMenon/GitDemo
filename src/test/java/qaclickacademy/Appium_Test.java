f76b5a8f836aa77036ec47917103648ebf0aaa16
hareesh <achuarem@gmail.com>
2022-08-14 16:18:36 +0200

    This is my first commit

---
+++ src/test/java/qaclickacademy/Appium_Test.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class Appium_Test 
+{
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");
+	}
+	
+	@Test
+	public void iOSApps()
+	{
+		System.out.println("iOSApps");
+	}
+}
---
+++ src/test/java/qaclickacademy/Selenium_Test.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class Selenium_Test 
+{
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("Selenium Test");
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+}
